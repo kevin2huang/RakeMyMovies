83d72357c3aa55c93d8ed351b29d39ad9ee05ca5
Kevin Huang <kevin2huang@hotmail.com>
2016-03-21 00:05:42 -0400

    edited test.sql

--- BackEnd/test.sql
+++ BackEnd/test.sql
@@ -36,31 +36,31 @@
 -- STUDIO_NAME TEXT,
 -- STUDIO_COUNTRY TEXT);
 
--- ***************STUDIO INSERT**************
-INSERT INTO STUDIO (STUDIO_NAME, STUDIO_COUNTRY)
-VALUES
-('Pixar', 'USA')
+-- ***************STUDIO INSERT****************************************************
+-- INSERT INTO STUDIO (STUDIO_NAME, STUDIO_COUNTRY)
+-- VALUES
+-- ('Pixar', 'USA')
+-- ********************************************************************************
 
--- ***************ACTOR INSERT***************
+-- ***************ACTOR INSERT****************************************************
 -- INSERT INTO ACTOR (ACTOR_NAME, ACTOR_DOB)
 -- VALUES
 -- ('Brad Pitt', 'March 3 1970')
--- *****************************************
+-- ******************************************************************************
 
--- ***************DIRECTOR INSERT************
+-- ***************DIRECTOR INSERT*************************************************
 -- INSERT INTO DIRECTOR (DIR_NAME, DIR_COUNTRY)
 -- VALUES
 -- ('Frank Darabont', 'USA')
--- 
--- *****************************************
+-- ******************************************************************************
 
--- ***************GENRE INSERTS**************
+-- ***************GENRE INSERTS***************************************************
 -- INSERT INTO GENRE (GENRE)
 -- VALUES
 -- ('Action');
--- ******************************************
+-- *******************************************************************************
 
--- **************MOVIE INSERTS***************
+-- **************MOVIE INSERTS****************************************************
 
 -- INSERT INTO MOVIES (MOVIE_TITLE, RELEASE_DATE, DESCRIPTION, TG_RATING)
 -- VALUES 
@@ -72,15 +72,9 @@ VALUES
 -- ('DeadPool', 'February 12, 2016', 'This is the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life.',
 -- 'Rated R');
 
--- SELECT * FROM MOVIES;
-
 -- INSERT INTO MOVIES (MOVIE_TITLE, RELEASE_DATE, DESCRIPTION, TG_RATING)
 -- VALUES 
 -- ('Avengers', 'May 4, 2012', 'The Asgardian Loki encounters the Other, the leader of an extraterrestrial race known as the Chitauri. In exchange for retrieving the Tesseract,2 a powerful energy source of unknown potential, the Other promises Loki an army with which he can subjugate Earth. Nick Fury, director of the espionage agency S.H.I.E.L.D., and his lieutenant Agent Maria Hill arrive at a remote research facility during an evacuation, where physicist Dr. Erik Selvig is leading a research team experimenting on the Tesseract. Agent Phil Coulson explains that the object has begun radiating an unusual form of energy. The Tesseract suddenly activates and opens a wormhole, allowing Loki to reach Earth. Loki takes the Tesseract and uses his scepter to enslave Selvig and a couple of agents, including Clint Barton, to aid him in his getaway.',
 -- 'PG-13');
-
--- ALTER TABLE MOVIES
--- 	ALTER COLUMN DESCRIPTION TYPE TEXT;
-
--- ********************************************
+-- *********************************************************************************
 
