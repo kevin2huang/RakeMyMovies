b8f9257e5d99022a58b5ec807aaaac784415d5e3
Kevin Huang <kevin2huang@hotmail.com>
2016-04-05 00:11:41 -0400

    edited getMovies.php

--- BackEnd/ajax/getMovies.php
+++ BackEnd/ajax/getMovies.php
@@ -1,12 +1,93 @@
 <?php
+ $host        = "host=127.0.0.1";
+   $port        = "port=5432";
+   $dbname      = "dbname=postgres";
+   $credentials = "user=postgres password=root";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      //echo "Opened database successfully\n";
+      pg_query('SET search_path = "Test"');
+   }
+
+//array of movie IDs
 header("content-type:application/json");
+
+$movies = array();
+
 if(isset($_POST['movieIDs'])){
 
-    $response = array();
+    $movie = array();
+    $actors = array();
+    $directors = array();
 
     foreach ($_POST['movieIDs'] as $movieID) {
+         $ret = pg_query($db, "SELECT M.*, A.*, D.*, S.*
+                                 FROM MOVIES M, ACTOR A, MOVACT MA, DIRECTOR D, MOVDIR MD, STUDIO S, SPONSOR SP
+                                 WHERE M.MOVIE_ID = " + $movieID + " AND
+                                        A.ACTOR_ID = MA.ACTOR_ID AND
+                                        MA.MOVIE_ID = M.MOVIE_ID AND
+                                        D.DIRECTOR_ID = MD.DIRECTOR_ID AND 
+                                        MD.MOVIE_ID = MOVIE_ID AND 
+                                        SP.MOVIE_ID = M.MOVIE_ID AND 
+                                        SP.STUDIO_ID = S.STUDIO_ID;")
+         /*
+         $ret2 = pg_query($db, "SELECT * 
+                                FROM ACTOR A
+                                WHERE A.ACTOR_ID = (SELECT MA.ACTOR_ID 
+                                FROM MOVACT MA, MOVIES M
+                                WHERE M.MOVIE_ID = " + $movieid +" AND MA.MOVIE_ID = M.MOVIE_ID);");
+
+         $ret3 = pg_query($db, "SELECT * 
+                                FROM DIRECTOR D
+                                WHERE D.DIRECTOR_ID = (SELECT MD.DIRECTOR_ID 
+                                FROM MOVDIR MD, MOVIES M
+                                WHERE M.MOVIE_ID = " + $movieid + " AND MD.MOVIE_ID = M.MOVIE_ID);");
+
+         $ret4 = pg_query($db, "SELECT * 
+                                FROM DIRECTOR D
+                                WHERE D.DIRECTOR_ID = (SELECT MD.DIRECTOR_ID 
+                                FROM MOVDIR MD, MOVIES M
+                                WHERE M.MOVIE_ID = " + $movieid + " AND MD.MOVIE_ID = M.MOVIE_ID);");
+*/
+    if(!$ret or !$ret2){
+      echo pg_last_error($db);
+      exit;
+    } 
+
+
+
+   while($row3 = pg_fetch_row($ret3)){
+        $directors = array('directorid' => $row3[0],
+                            'directorname' => $row[1]);
+   } 
+
+   while($row2 = pg_fetch_row($ret2)){
+        $actors = array('actorid' => $row2[0],
+                        'actorname' => $row2[1]);
+   }
+    
+    while($row = pg_fetch_row($ret))
+   {
+            $movie = array('movieid' => $row[0], 
+                                 'movietitle' => $row[1], 
+                                 'moviecover' => $row[2],
+                                 'moviereleasedate' => $row[3], 
+                                 'moviedescription' => $row[4],
+                                 'movieduration' => $row[5],
+                                 'movielanguage' => $row[6],
+                                 'moviecountry' => $row[7],
+                                 'movietgrating' => $row[8],
+                                 'actors' => $actors,
+                                 'directors' => $directors);
+    }
+
         //TODO Replace the following with a database check
         $elem = array(
+            'id' => '1'
             'name' => 'This is a movie',
             'time' => '1h30',
             'rating' => 5,
