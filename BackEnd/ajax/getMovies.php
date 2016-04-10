ffe617f7210b2d656c4a952ee78ffb5886565fab
grimGrendel <pascaltl@hotmail.com>
2016-04-10 17:34:55 -0400

    Search is fully operational

--- BackEnd/ajax/getMovies.php
+++ BackEnd/ajax/getMovies.php
@@ -11,47 +11,41 @@
       pg_query('SET search_path = "RakeMyMovie";');
    }
 
- $_POST['userId'] = 10;
- $_POST['listType'] = 'wish';
+function makeMovieNoTS($ids, $database)
+  {  
+    $movies = array();
+    foreach ($ids as $key=>$value) {
+          $movie = array();
+          $actors = array();
+          $directors = array();
+          $studios = array();
 
-if(isset($_POST['userId']))
-{
+         $ret = pg_query($database, "SELECT * 
+                                FROM MOVIES
+                                WHERE MOVIE_ID = " . $ids[$key] . ";");
+         
+         $ret2 = pg_query($database, "SELECT A.* 
+                                FROM ACTOR A, MOVACT MA, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      MA.MOVIE_ID = M.MOVIE_ID AND
+                                      MA.ACTOR_ID = A.ACTOR_ID;");
 
-    function makeMovieNoTS($ids, $database)
-    {  
-      $movies = array();
-      foreach ($ids as $key=>$value) {
-            $movie = array();
-            $actors = array();
-            $directors = array();
-            $studios = array();
+         $ret3 = pg_query($database, "SELECT D.* 
+                                FROM DIRECTOR D, MOVDIR MD, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      MD.MOVIE_ID = M.MOVIE_ID AND
+                                      MD.DIR_ID = D.DIR_ID;");
 
-           $ret = pg_query($database, "SELECT * 
-                                  FROM MOVIES
-                                  WHERE MOVIE_ID = " . $ids[$key] . ";");
-           
-           $ret2 = pg_query($database, "SELECT A.* 
-                                  FROM ACTOR A, MOVACT MA, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        MA.MOVIE_ID = M.MOVIE_ID AND
-                                        MA.ACTOR_ID = A.ACTOR_ID;");
+         $ret4 = pg_query($database, "SELECT S.* 
+                                FROM STUDIO S, SPONSOR SP, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      SP.MOVIE_ID = M.MOVIE_ID AND
+                                      SP.STUDIO_ID = S.STUDIO_ID;");
 
-           $ret3 = pg_query($database, "SELECT D.* 
-                                  FROM DIRECTOR D, MOVDIR MD, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        MD.MOVIE_ID = M.MOVIE_ID AND
-                                        MD.DIR_ID = D.DIR_ID;");
-
-           $ret4 = pg_query($database, "SELECT S.* 
-                                  FROM STUDIO S, SPONSOR SP, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        SP.MOVIE_ID = M.MOVIE_ID AND
-                                        SP.STUDIO_ID = S.STUDIO_ID;");
-
-      if(!$ret or !$ret2 or !$ret3 or !$ret4){
-        echo pg_last_error($database);
-        exit;
-      } 
+    if(!$ret or !$ret2 or !$ret3 or !$ret4){
+      echo pg_last_error($database);
+      exit;
+    } 
 
     while($row4 = pg_fetch_row($ret4)){
           array_push($studios, array('studioid' => $row4[0],
@@ -84,83 +78,85 @@ if(isset($_POST['userId']))
                              'studios' => $studios);
       }
        array_push($movies, $movie);
-      }
-      return $movies;
     }
+      return $movies;
+}
+function makeMovieWithTS($ids, $database, $ts)
+  {  
+    $movies = array();
+    foreach ($ids as $key=>$value) {
+          $movie = array();
+          $actors = array();
+          $directors = array();
+          $studios = array();
 
-    function makeMovieWithTS($ids, $database, $ts)
-    {  
-      $movies = array();
-      foreach ($ids as $key=>$value) {
-            $movie = array();
-            $actors = array();
-            $directors = array();
-            $studios = array();
+         $ret = pg_query($database, "SELECT * 
+                                FROM MOVIES
+                                WHERE MOVIE_ID = " . $ids[$key] . ";");
+         
+         $ret2 = pg_query($database, "SELECT A.* 
+                                FROM ACTOR A, MOVACT MA, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      MA.MOVIE_ID = M.MOVIE_ID AND
+                                      MA.ACTOR_ID = A.ACTOR_ID;");
 
-           $ret = pg_query($database, "SELECT * 
-                                  FROM MOVIES
-                                  WHERE MOVIE_ID = " . $ids[$key] . ";");
-           
-           $ret2 = pg_query($database, "SELECT A.* 
-                                  FROM ACTOR A, MOVACT MA, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        MA.MOVIE_ID = M.MOVIE_ID AND
-                                        MA.ACTOR_ID = A.ACTOR_ID;");
+         $ret3 = pg_query($database, "SELECT D.* 
+                                FROM DIRECTOR D, MOVDIR MD, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      MD.MOVIE_ID = M.MOVIE_ID AND
+                                      MD.DIR_ID = D.DIR_ID;");
 
-           $ret3 = pg_query($database, "SELECT D.* 
-                                  FROM DIRECTOR D, MOVDIR MD, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        MD.MOVIE_ID = M.MOVIE_ID AND
-                                        MD.DIR_ID = D.DIR_ID;");
+         $ret4 = pg_query($database, "SELECT S.* 
+                                FROM STUDIO S, SPONSOR SP, MOVIES M
+                                WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
+                                      SP.MOVIE_ID = M.MOVIE_ID AND
+                                      SP.STUDIO_ID = S.STUDIO_ID;");
 
-           $ret4 = pg_query($database, "SELECT S.* 
-                                  FROM STUDIO S, SPONSOR SP, MOVIES M
-                                  WHERE M.MOVIE_ID = " . $ids[$key] . " AND 
-                                        SP.MOVIE_ID = M.MOVIE_ID AND
-                                        SP.STUDIO_ID = S.STUDIO_ID;");
+    if(!$ret or !$ret2 or !$ret3 or !$ret4){
+      echo pg_last_error($database);
+      exit;
+    } 
 
-      if(!$ret or !$ret2 or !$ret3 or !$ret4){
-        echo pg_last_error($database);
-        exit;
-      } 
-
-     while($row4 = pg_fetch_row($ret4)){
-          $studios = array('studioid' => $row4[0],
-                              'studioname' => $row4[1]);
+   while($row4 = pg_fetch_row($ret4)){
+          array_push($studios, array('studioid' => $row4[0],
+                              'studioname' => $row4[1]));
      }     
 
      while($row3 = pg_fetch_row($ret3)){
-          $directors = array('directorid' => $row3[0],
-                              'directorname' => $row3[1]);
+          array_push($directors, array('directorid' => $row3[0],
+                              'directorname' => $row3[1]));
      } 
 
      while($row2 = pg_fetch_row($ret2)){
-          $actors = array('actorid' => $row2[0],
-                          'actorname' => $row2[1]);
+          array_push($actors, array('actorid' => $row2[0],
+                          'actorname' => $row2[1]));
      }
-      
-      while($row = pg_fetch_row($ret))
-     {
-              $movie = array('movieid' => $row[0], 
-                             'movietitle' => $row[1], 
-                             'moviecover' => $row[2],
-                             'moviereleasedate' => $row[3], 
-                             'moviedescription' => $row[4],
-                             'movieduration' => $row[5],
-                             'movielanguage' => $row[6],
-                             'moviecountry' => $row[7],
-                             'movietgrating' => $row[8],
-                             'actors' => $actors,
-                             'directors' => $directors,
-                             'studios' => $studios,
-                             'timestamp' => $ts[$key]);
-      }
-       array_push($movies, $movie);
-      }
-      return $movies;
+    
+    while($row = pg_fetch_row($ret))
+   {
+            $movie = array('movieid' => $row[0], 
+                           'movietitle' => $row[1], 
+                           'moviecover' => $row[2],
+                           'moviereleasedate' => $row[3], 
+                           'moviedescription' => $row[4],
+                           'movieduration' => $row[5],
+                           'movielanguage' => $row[6],
+                           'moviecountry' => $row[7],
+                           'movietgrating' => $row[8],
+                           'actors' => $actors,
+                           'directors' => $directors,
+                           'studios' => $studios,
+                           'timestamp' => $ts[$key]);
     }
-  
-  $movieids = array();
+     array_push($movies, $movie);
+    }
+    return $movies;
+  }
+
+$movieids = array();
+
+if(isset($_POST['userId']))
+{  
   $timestamps = array();
 
   if ($_POST['listType'] === 'wish') 
@@ -179,6 +175,7 @@ if(isset($_POST['userId']))
 
     $mmovies = makeMovieWithTS($movieids, $db, $timestamps);
   }
+}
   else if($_POST['listType'] === 'watched')
   {
     $list = pg_query($db, "SELECT M.*, W.WATCHED_TIMESTAMP
@@ -195,45 +192,45 @@ if(isset($_POST['userId']))
     $mmovies = makeMovieWithTS($movieids, $db, $timestamps);
   }
   else if (isset($_POST['movieId']))
+{
+  $list = pg_query($db, "SELECT *
+                          FROM MOVIES 
+                          WHERE MOVIE_ID = " . $_POST['movieId'] . ";");
+
+  while ($row = pg_fetch_row($list))
   {
-    $list = pg_query($db, "SELECT *
-                            FROM MOVIES 
-                            WHERE MOVIE_ID = " . $_POST['movieId'] . ";");
-  
-    while ($row = pg_fetch_row($list))
-    {
-       array_push($movieids, $row[0]);
-    }
-    $mmovies = makeMovieNoTS($movieids, $db);
+     array_push($movieids, $row[0]);
   }
-  else if (isset($_POST['genreId']))
+  $mmovies = makeMovieNoTS($movieids, $db);
+}
+else if (isset($_POST['genreId']))
+{
+  $list = pg_query($db, "SELECT M.*
+                          FROM MOVIES M, GENRE G, MOVGEN MG
+                          WHERE G.GENRE_ID = " . $_POST['genreId'] . "AND
+                          G.GENRE_ID = MG.GENRE_ID AND 
+                          MG.MOVIE_ID = M.MOVIE_ID;");
+
+  while ($row = pg_fetch_row($list))
   {
-    $list = pg_query($db, "SELECT M.*
-                            FROM MOVIES M, GENRE G, MOVGEN MG
-                            WHERE G.GENRE_ID = " . $_POST['genreId'] . "AND
-                            G.GENRE_ID = MG.GENRE_ID AND 
-                            MG.MOVIE_ID = M.MOVIE_ID;");
-  
-    while ($row = pg_fetch_row($list))
-    {
-       array_push($movieids, $row[0]);
-    }
-    $mmovies = makeMovieNoTS($movieids, $db);
+     array_push($movieids, $row[0]);
   }
-  else if (isset($_POST['directorId']))
+  $mmovies = makeMovieNoTS($movieids, $db);
+}
+else if (isset($_POST['directorId']))
+{
+  $list = pg_query($db, "SELECT M.*
+                          FROM MOVIES M, DIRECTOR D, MOVDIR MD
+                          WHERE D.DIR_ID = " . $_POST['directorId'] . "AND
+                          D.DIR_ID = MD.DIR_ID AND
+                          MD.MOVIE_ID = M.MOVIE_ID;");
+
+  while ($row = pg_fetch_row($list))
   {
-    $list = pg_query($db, "SELECT M.*
-                            FROM MOVIES M, DIRECTOR D, MOVDIR MD
-                            WHERE D.DIR_ID = " . $_POST['directorId'] . "AND
-                            D.GIR_ID = MD.DIR_ID AND
-                            MD.MOVIE_ID = M.MOVIE_ID;");
-  
-    while ($row = pg_fetch_row($list))
-    {
-       array_push($movieids, $row[0]);
-    }
-    $mmovies = makeMovieNoTS($movieids, $db);
+     array_push($movieids, $row[0]);
   }
+  $mmovies = makeMovieNoTS($movieids, $db);
+}
   else
   {
     echo "ERROR";
@@ -241,9 +238,4 @@ if(isset($_POST['userId']))
 
    $allmovies = array('movies' => $mmovies);
    echo json_encode($allmovies);
-}
-else
-{
-  echo "ERROR";
-}
 ?>
