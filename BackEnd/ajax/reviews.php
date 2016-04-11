be7571c9dd7e379916dbb0d20b77fc3a21ea8a0b
Kevin Huang <kevin2huang@hotmail.com>
2016-04-10 23:23:09 -0400

    finished review.php
    needs to be tested.

--- BackEnd/ajax/reviews.php
+++ BackEnd/ajax/reviews.php
@@ -11,31 +11,115 @@
       pg_query('SET search_path = "RakeMyMovie";');
    }
 
+date_default_timezone_set('EST');
+$date = date('j F Y');
 
+//Homepage and search same usecase. 
 if(isset($_GET['userId']) and isset($_GET['movieId'])) {
+	$getreview = pg_query($db, "SELECT R.*
+								FROM REVIEW R, RAKEUSER U, USRREV UR, MOVREV MR
+								WHERE U.USER_ID = " . $_GET['userId'] . "AND 
+								U.USER_ID = UR.USER_ID AND
+								UR.REVIEW_ID = R.REVIEW_ID AND 
+								R.REVIEW_ID = MR.REVIEW_ID AND 
+								MR.MOVIE_ID = " . $_GET['movieId'] . ";");
+	if(!$getreview){
+		echo "EMPTY";
+	}
+	else
+	{
+		while($row = pg_fetch_row($getreview)){
+			$thereview = array('reviewid' => $row[0],
+								'reviewdescription' => $row[1],
+								'reviewrating' => $row[2],
+								'reviewdate' => $row[3]);
+		}
+	}
+	
+	echo json_encode($thereview);
 	/*
 	Get the rating and the description of the review between that user and that movie. If none
 	are found, return the string 'EMPTY'
 	*/
-} else if (isset($_GET['userId'])) {
-	/*
-	No movieId was passed, only the userId. Return every review written by this user
-	*/
+} 
+/* No movieId was passed, only the userId. Return every review written by this user*/
+else if (isset($_GET['userId'])) {
+	$reviews = array();
+	$getreviews = pg_query($db, "SELECT DISTINCT R.*
+								FROM REVIEW R, RAKEUSER U, USRREV UR, MOVREV MR
+								WHERE U.USER_ID = " . $_GET['userId'] . " AND 
+								U.USER_ID = UR.USER_ID AND
+								UR.REVIEW_ID = R.REVIEW_ID;");
+	if(!$getreviews){
+		pg_last_error($db);
+		exit;
+	}
+
+	while($row = pg_fetch_row($getreviews)){
+		array_push($reviews, array('reviewid' => $row[0],
+							'reviewdescription' => $row[1],
+							'reviewrating' => $row[2],
+							'reviewdate' => $row[3]));
+	}
+	$rreviews = array('reviews' => $reviews);
+	echo json_encode($rreviews);
+}
+else
+{
+	echo "EMPTY";
 }
 
 if( isset($_POST['userId']) and isset($_POST['movieId']) and 
-	isset($_POST['rating']) and isset($_POST['text'])) {
+	isset($_POST['rating']) and isset($_POST['text'])) 
+{
 
-	if( isset($_POST['update']) and $_POST['update']) {
-		/*
-		If $_POST_['update'] is true, then a review already exists in the database between this user and that moview. Update its rating and description values	
-		*/
-	} else {
-		/*
-		Else, then no review previously exists between this user and that movie. Create a new review relation
-		*/
+	/*If $_POST_['update'] is true, then a review already exists in the database between this user and that movie. Update its rating and description values*/
+	if( isset($_POST['update']) and $_POST['update'])
+	{
+		$update_review = pg_query($db, "UPDATE REVIEW AS R
+										SET REVIEW_DESCRIPTION = ". $_POST['text'] . ",
+										REVIEW_RATING = " . $_POST['rating'] . ", 
+										FROM MOVIES M, MOVREV MR, RAKEUSER U, USRREV UR
+										WHERE M.MOVIE_ID = " . $_POST['movieId'] ." AND
+										MR.MOVIE_ID = M.MOVIE_ID AND
+										MR.REVIEW_ID = R.REVIEW_ID;");
+		if(!$update_review){
+			echo pg_last_error($db);
+			exit;
+		}
+		echo "OK";
+	} 
+	/*Else, then no review previously exists between this user and that movie. Create a new review relation*/
+	else 
+	{
+		$create_review = pg_query($db, "INSERT INTO REVIEW (REVIEW_DESCRIPTION, REVIEW_RATING, REVIEW_DATE)
+										VALUES 
+										('" . $_POST['text'] . "'," . $_POST['rating'] ", '" . $date . "');");
+
+		while($row = pg_fetch_row($create_review)){
+			$reviewid = $row[0];
+		}
+
+		$create_user_review = pg_query($db, "INSERT INTO USRREV 
+											 VALUES 
+											 (" . $_POST['userId'] . ", " . $reviewid . ");");
+
+		$create_movie_review = pg_query($db, "INSERT INTO MOVREV
+											  VALUES 
+											  (" . $_POST['movieId'] . ", " . $reviewid . ");");
+
+		if(!$create_review or !$create_user_review or !$create_movie_review){
+			echo pg_last_error($db);
+			exit;
+		}
+
+		echo "OK";
 	}
 	
+}
+else
+{
+	echo "FAILED";
 } 
 
 /*$response = array();
@@ -45,7 +129,4 @@ if( isset($_POST['userId']) and isset($_POST['movieId']) and
  		'rating': 5,
  		'name': 'Anonymous'
  		);*/
-}
-
-    echo json_encode($response);
 ?>
